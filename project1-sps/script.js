119da902e5776a8b241e22b215c3eddc59acad36
andrewatjoa <100643551+andrewatjoa@users.noreply.github.com>
2022-04-14 21:43:55 +0800

    This is my first time trying to make a pull request I redid everything to make sure that I've done everything properly

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,223 @@
+// Global Variables to set initial game states
+
+var currentGameMode = `waiting for user name`;
+
+// Create a global variable called User Name: ask user to enter user name
+var userName = ``;
+
+var spsMode = ``;
+
+//Track number of games played
+var numGamesPlayed = 0;
+
+//Track number of games won
+var numGamesWon = 0;
+
+var numGamesLost = 0;
+
+var numGamesDrawn = 0;
+
+var getWinRateInfo = function () {
+  return `Your current win rate is ${Math.floor(
+    (numGamesWon / numGamesPlayed) * 100
+  )} %`;
+};
+
+// Since the object is always reused, it's very tedious to type and is prone to typo, can I store it as a variable?
+
+var scissors = "scissors";
+var paper = "paper";
+var stone = "stone";
+var revScissors = "reversed scissors";
+var revPaper = "reversed paper";
+var revStone = "reversed stone";
+
+// Create a helper function that generates "scissors" "paper" and "stone" randomly.
+
+var genSPSRandom = function () {
+  var randomInteger = Math.ceil(Math.random() * 3);
+  if (randomInteger == 1) {
+    return scissors;
+  }
+  if (randomInteger == 2) {
+    return paper;
+  }
+  if (randomInteger == 3) {
+    return stone;
+  }
+};
+
+// Create a helper function that generates the emoji that symbolizes scissors, paper and stone respectively.
+
+var genSPSEmoji = function (SPS) {
+  if (SPS == scissors || SPS == revScissors) {
+    return "✂️";
+  }
+  if (SPS == paper || SPS == revPaper) {
+    return "📝";
+  }
+
+  if (SPS == stone || SPS == revStone) {
+    return "💎";
+  }
+};
+
+// Create helper functions that contains win, lose or draw conditions
+var drawCondition = function (playerThrow, computerThrow) {
+  return (
+    playerThrow == computerThrow ||
+    (playerThrow == revScissors && computerThrow == scissors) ||
+    (playerThrow == revPaper && computerThrow == paper) ||
+    (playerThrow == revStone && computerThrow == stone)
+  );
+};
+
+var winConditionNormalMode = function (playerThrow, computerThrow) {
+  return (
+    (playerThrow == scissors && computerThrow == paper) ||
+    (playerThrow == stone && computerThrow == scissors) ||
+    (playerThrow == paper && computerThrow == stone)
+  );
+};
+
+var winConditionReverseMode = function (playerThrow, computerThrow) {
+  return (
+    (playerThrow == revScissors && computerThrow == stone) |
+      (playerThrow == revStone && computerThrow == paper) ||
+    (playerThrow == revPaper && computerThrow == scissors)
+  );
+};
+
+var myOutputValue = ``;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // Create a game mode for users to enter their user name and invite them to choose a game mode as an output.
+
+  console.log("The current game mode is", currentGameMode);
+  if (currentGameMode == `waiting for user name`) {
+    if (!input) {
+      return `Please input your user name to play the game!`;
+    }
+    userName = input;
+    currentGameMode = "choose game mode";
+
+    return (
+      `Hello ` +
+      userName +
+      `! Welcome to Andrew's scissors paper stone game!` +
+      `<br>` +
+      `Please enter 1 to play the normal mode.` +
+      `<br>` +
+      `Please enter 2 to play the reverse mode.`
+    );
+  }
+  console.log("Currently the game mode is", currentGameMode);
+
+  if (currentGameMode == `choose game mode`) {
+    spsMode = input;
+
+    if (input == "1") {
+      currentGameMode = `sps normal mode`;
+
+      return (
+        `You've chosen the normal mode!` +
+        `<br>` +
+        `Please input scissors, paper or stone to play the game!`
+      );
+    } else if (input == "2") {
+      currentGameMode = `sps reverse mode`;
+
+      return (
+        `You've chosen the reverse mode!` +
+        `<br>` +
+        `Please input reversed scissors, reversed paper or reversed stone to play the game!`
+      );
+    } else {
+      return `Please input either "1" or "2" to choose a game mode`;
+    }
+  }
+  console.log("The current game mode is ", currentGameMode);
+  if (currentGameMode == `sps normal mode`) {
+    console.log("The current game is ", currentGameMode);
+    console.log("The current number of games played is:", numGamesPlayed);
+
+    var playerThrows = input;
+
+    var computerPlayed = genSPSRandom();
+    console.log("The computer played", computerPlayed);
+    var playerEmoji = genSPSEmoji(input);
+    var computerEmoji = genSPSEmoji(computerPlayed);
+
+    // Validate input: if input is not scissors, paper or stone, instruct user to type in scissors, paper or stone to play the game.
+    if (
+      playerThrows != scissors &&
+      playerThrows != paper &&
+      playerThrows != stone
+    ) {
+      return `Please type in scissors, paper or stone to play the game`;
+    }
+    // increase the number of games played by 1 if input is correct
+    numGamesPlayed += 1;
+    // Check if the game results in a draw
+    if (drawCondition(playerThrows, computerPlayed)) {
+      numGamesDrawn += 1;
+      var currentWinRate = getWinRateInfo();
+      return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> It's a draw! 🤝 <br> Please input scissors, paper, and stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+    }
+
+    // Check if the game results in the user winning
+    else if (winConditionNormalMode(playerThrows, computerPlayed)) {
+      numGamesWon += 1;
+      console.log("Your current number of games won is:", numGamesWon);
+      currentWinRate = getWinRateInfo();
+      return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> You won! Congratulations! 🎉 <br> Please input scissors, paper, and stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+    }
+    // If it's neither a win nor a draw, it's a loss.
+    else numGamesLost += 1;
+    currentWinRate = getWinRateInfo();
+    return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> You lost! 😭  Better luck next time! <br> Please input scissors, paper, and stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+  }
+
+  // Check if the user chooses the reverse mode, and define the game logic
+  if (currentGameMode == `sps reverse mode`) {
+    console.log("The current game is ", currentGameMode);
+    console.log("The current number of games played is:", numGamesPlayed);
+
+    playerThrows = input;
+    currentWinRate = getWinRateInfo();
+    computerPlayed = genSPSRandom();
+    console.log("The computer played", computerPlayed);
+    playerEmoji = genSPSEmoji(input);
+    computerEmoji = genSPSEmoji(computerPlayed);
+
+    // Validate input: if input is not scissors, paper or stone, instruct user to type in scissors, paper or stone to play the game.
+
+    if (
+      playerThrows != revScissors &&
+      playerThrows != revPaper &&
+      playerThrows != revStone
+    ) {
+      return `Please type in reversed scissors, reversed paper or reversed stone to play the game`;
+    }
+    // increase the number of games played by 1 if input is correct
+    numGamesPlayed += 1;
+    // Check if the game results in a draw
+    if (drawCondition(playerThrows, computerPlayed)) {
+      numGamesDrawn += 1;
+      currentWinRate = getWinRateInfo();
+      return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> It's a draw! 🤝 <br> Please input reversed paper, reversed scissors, and reversed stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+    }
+
+    // Check if the game results in the user winning
+    else if (winConditionReverseMode(playerThrows, computerPlayed)) {
+      numGamesWon += 1;
+      currentWinRate = getWinRateInfo();
+      console.log("Your current number of games won is:", numGamesWon);
+      return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> You won! Congratulations! 🎉  <br> Please input reversed paper, reversed scissors, and reversed stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+    }
+    // If it's neither a win or a draw, then it's a loss.
+    else numGamesLost += 1;
+    currentWinRate = getWinRateInfo();
+    return `${userName} threw: ${playerThrows} ${playerEmoji} <br> Computer played: ${computerPlayed} ${computerEmoji} <br><br> You lost! 😭  Better luck next time! <br> Please input reversed paper, reversed scissors, and reversed stone to continue playing! <br><br> Your current win/loss record is: <br> ${userName} won: ${numGamesWon} times. <br> Computer won:  ${numGamesLost} times. <br> You both drew: ${numGamesDrawn} times. <br> ${currentWinRate}`;
+  }
 };
--- project2-beat-that/script.js
+++ project2-beat-that/script.js
@@ -1,4 +1,159 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+// Declaring global variables that determines game state
+
+const gameStateDiceRoll = `game state dice roll`;
+const gameStateChooseDiceOrder = `game state choose dice order`;
+const gameStateCompareScores = `game state compare scores`;
+var gameState = gameStateDiceRoll;
+
+// Create an array to store the rolled dice values
+var currentPlayerRolls = [];
+
+var currentPlayer = 1;
+var allPlayersScore = [0, 0];
+
+var resetGame = function () {
+  currentPlayer = 1;
+  gameState = gameStateDiceRoll;
 };
+
+// helper function to roll dice
+
+var rollDice = function () {
+  console.log(`Control flow: start of rollDice()`);
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.ceil(randomDecimal);
+  console.log(`the roll dice output is `, randomInteger);
+  return randomInteger;
+};
+
+// create a helper function that will roll two dice and return a message
+
+var rollDiceForPlayer = function () {
+  console.log(`Control Flow: start of rollDiceForPlayer()`);
+  var counter = 0;
+  while (counter < 2) {
+    currentPlayerRolls.push(rollDice());
+    counter += 1;
+  }
+  console.log(
+    `rollDiceForPlayer changes, currentPlayerRolls: `,
+    currentPlayerRolls
+  );
+  return `Welcome, Player ${currentPlayer}! <br><br> You rolled: <br> Dice 1: ${currentPlayerRolls[0]} <br> Dice 2: ${currentPlayerRolls[1]} <br><br> Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value.`;
+};
+
+var getPlayerScore = function (playerInput) {
+  var playerScore;
+  // input validation
+  if (playerInput != 1 && playerInput != 2) {
+    return `Error! Please only input "1" or "2" to choose which dice to use as the first digit. <br><br> Your dice rolls are: <br> Dice 1: ${currentPlayerRolls[0]} <br> Dice 2: ${currentPlayerRolls[1]}`;
+  }
+  // if player input "1"
+  if (playerInput == 1) {
+    console.log(`Control flow: input == 1`);
+
+    playerScore = Number(
+      String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+    );
+    console.log(`current player score is: `, playerScore);
+  }
+  // if player input "2"
+  if (playerInput == 2) {
+    console.log(`Control flow: input == 2`);
+    playerScore = Number(
+      String(currentPlayerRolls[1]) + String(currentPlayerRolls[0])
+    );
+    console.log(`current playerScore is: `, playerScore);
+  }
+  // Store playerScore in array
+  allPlayersScore[currentPlayer - 1] += playerScore;
+  //Clear current player rolls array
+  currentPlayerRolls = [];
+  return `Player ${currentPlayer}, Your chosen value is: ${playerScore}.`;
+};
+
+var comparePlayerScores = function () {
+  var compareMessage = `Player 1 scores: ${allPlayersScore[0]} <br> Player 2 scores: ${allPlayersScore[1]}`;
+  console.log(allPlayersScore);
+  // player 1 wins
+  if (allPlayersScore[0] > allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br> <br> Player 1 wins!`;
+  }
+  // player 2 wins
+  if (allPlayersScore[0] < allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br> <br> Player 2 wins!`;
+  }
+  // tie
+  if (allPlayersScore[0] == allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br> <br> It's a tie!`;
+  }
+  return compareMessage;
+};
+
+var main = function (input) {
+  console.log(
+    `Checking game state when the submit button is clicked: `,
+    gameState
+  );
+  console.log(`Checking currentPlayer on submit click: `, currentPlayer);
+  var myOutputValue = "";
+  if (gameState == gameStateDiceRoll) {
+    console.log(`Control flow: gameState == gameStateDiceRoll`);
+    // Change the game state
+    gameState = gameStateChooseDiceOrder;
+    return rollDiceForPlayer();
+  }
+
+  if (gameState == gameStateChooseDiceOrder) {
+    console.log(`Control flow: gameState == gameStateChooseDiceOrder`);
+
+    // Call playerScore function
+    myOutputValue = getPlayerScore(input);
+
+    if (input != 1 && input != 2) {
+      return `Error! Please only input "1" or "2" to choose which dice to use as the first digit. <br><br> Your dice rolls are: <br> Dice 1: ${currentPlayerRolls[0]} <br> Dice 2: ${currentPlayerRolls[1]}`;
+    } else if (currentPlayer == 1) {
+      console.log(
+        `Control flow: end of player 1's turn, now it's player 2's turn.`
+      );
+      currentPlayer = 2;
+      gameState = gameStateDiceRoll;
+      return `${myOutputValue} <br><br> It is now player 2's turn!"`;
+    } else if (currentPlayer == 2) {
+      console.log(
+        `Control flow: end of player 2's turn. Next, submit click will calculate score`
+      );
+      console.log(`The current allPlayerScores array is `, allPlayersScore);
+      gameState = gameStateCompareScores;
+      return `${myOutputValue} <br><br> Press submit to compare scores`;
+    }
+  }
+
+  if (gameState == gameStateCompareScores) {
+    console.log(`Control flow: gameState == gameStateCompareScores`);
+    console.log(`The current allPlayerScores array is `, allPlayersScore);
+
+    var outputMessage = comparePlayerScores();
+
+    resetGame();
+    console.log(`Current player after reset: `, currentPlayer);
+    console.log(`Game state after reset: `, gameState);
+    console.log(`allPlayersScore array: `, allPlayersScore);
+    return outputMessage;
+  }
+};
+
+//----------REQUIREMENTS------------//
+// 1) there are 2 players and players take turnns
+// 2) when a player clicks submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
+// 3) the player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the second dice goes first
+// 4) After both players have rolled and chosen dice order, the player with the higher combined number wins.
+//----------PROBLEMS BREAKDOWN AND PLANNING-----------//
+// ver 1: rolls 2 dice and turns the output for 1 player. That player chooses the dice order and get the correct return output.
+// ver 2: refactor code to include player 2
+//    - global variables for currentPlayer; allPlayersScore
+//    - refactor outputMessages to interact with each, player 1 and 2
+//    - write logic for player 1 to go first and then player 2, and finally point towards comparing score
+
+// ver 3: implement comparing dice scores and declare winner
+// ver 4. reset the game so that the players can play continually without refreshing the browser page
